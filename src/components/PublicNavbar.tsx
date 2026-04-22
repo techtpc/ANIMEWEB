849eedfe1ce525b13ea8aa52a055980f452dbd95
techTPC <tech@thepitchcreative.media>
2026-04-22 13:02:40 +0700

    ganti logo

--- src/components/PublicNavbar.tsx
+++ src/components/PublicNavbar.tsx
@@ -1,14 +1,24 @@
 import Link from 'next/link';
 import SearchBox from '@/components/SearchBox';
+import Image from 'next/image';
 
 export default function PublicNavbar() {
   return (
-    <nav className="bg-[#141519] border-b border-gray-800 p-4 sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
-        <div className="flex items-center w-full md:w-auto justify-between">
-          <Link href="/" className="text-2xl font-black text-blue-500 italic">
-            SAMEHADAKU<span className="text-white font-normal not-italic">CLONE</span>
+    <nav className="bg-[#141519] border-b border-gray-800 py-2 px-4 md:py-2 md:px-0 sticky top-0 z-50">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center gap-2 md:gap-4">
+        <div className="flex items-center w-full md:w-auto justify-start">
+          <Link href="/" className="relative block group">
+            <div className="relative w-[200px] h-[40px] sm:w-[240px] sm:h-[52px] md:w-[300px] md:h-[70px] lg:w-[320px] lg:h-[80px] transition-transform duration-300 group-hover:scale-105">
+           <Image
+             src="/logo.png"
+             alt="Logo"
+             fill
+             priority
+             className="object-contain object-left"
+           />
+           </div>
           </Link>
+
         </div>
 
         <div className="flex flex-col md:flex-row items-center gap-6 w-full md:w-auto justify-between">

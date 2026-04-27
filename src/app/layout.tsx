c85ee117dcc4031629c762f324b4ff6795b76137
techTPC <tech@thepitchcreative.media>
2026-04-27 16:52:29 +0700

    add google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="id">
       <head>
-        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9348337344283565" crossOrigin="anonymous"></Script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9348337344283565" crossOrigin="anonymous"></script>
       </head>
       <body className={inter.className}>
         {/* Kamu bisa pasang Navbar di sini nanti */}
